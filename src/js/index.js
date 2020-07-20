84fca4b674d653acd680d4c9bc705e1987b16085
rachidbend <rachidbendaghor@gmail.com>
2020-07-20 12:19:36 +0100

    first commit

---
+++ src/js/index.js
@@ -0,0 +1,8 @@
+import './../sass/main.scss';
+
+
+console.log('i love afaf, i want to marry her');
+
+
+
+
---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path');
+
+module.exports = {
+    entry: './src/js/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'js/bundle.js'
+    },
+    mode: 'development',
+    devServer: {
+        contentBase: './dist'
+    },
+    module: {
+        rules: [
+          {
+            test: /\.s[ac]ss$/i,
+            use: [
+              // Creates `style` nodes from JS strings
+              'style-loader',
+              // Translates CSS into CommonJS
+              'css-loader',
+              // Compiles Sass to CSS
+              'sass-loader',
+            ],
+          },
+        ],
+      },
+
+
+};
