c32827e10c765ba8835cde7282127dc7562fa069
rachidbend <rachidbendaghor@gmail.com>
2020-07-22 01:37:33 +0100

    done the html and the styling for desktop and got question section working

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,57 @@
 import './../sass/main.scss';
 
 
-console.log('i love afaf, i want to marry her');
 
 
 
+let questionLabel = () => {
+
+// get a certen question
+    document.querySelector('.questions__container').addEventListener('click', event => {
+        let item = event.target.closest('.question');
+
+        // -----------------------------------
+        if (item) {
+            if (item.matches('.question')) {
+                if (item.lastElementChild.style.display === 'none') {
+                    item.lastElementChild.style.display = 'block';
+                    item.firstElementChild.lastElementChild.style.transform = 'rotate(180deg)';
+                    setTimeout(()=> {
+                        item.lastElementChild.classList.toggle('answer--active');
+                    }, 1)
+                    
+                } 
+                else if (item.lastElementChild.style.display === 'block') {
+                    item.lastElementChild.style.display = 'none';
+                    item.firstElementChild.lastElementChild.style.transform = 'rotate(0deg)';
+                    setTimeout(()=> {
+                        item.lastElementChild.classList.toggle('answer--active');
+                    }, 1)
+                } 
+                else if (!item.lastElementChild.style.display) {
+                    item.lastElementChild.style.display = 'block';
+                    item.firstElementChild.lastElementChild.style.transform = 'rotate(180deg)';
+                    setTimeout(()=> {
+                        item.lastElementChild.classList.toggle('answer--active');
+                    }, 1)
+                }
+            }
+        } 
+        // -----------------------------------
+
+
+
+
+    });
+
+   
+
+    
+// animate the answer of that question
+
+
+
+}
+
+questionLabel();
 
